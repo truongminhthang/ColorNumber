63ce465f0dc76275e3cff23c8ce7e18d1b89db0a
Thuy Truong Quang <tqthuy.ict@gmail.cm>
2017-12-26 12:46:00 +0700

    Done to create Location Folder

---
+++ ColorNumber/DataProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  DataProtocol.swift
+//  ColorNumber
+//
+//  Created by Thuy Truong Quang on 12/26/17.
+//  Copyright © 2017 BigZero. All rights reserved.
+//
+
+import Foundation
---
+++ ColorNumber/SupportingFiles/SupportingFiles.swift
@@ -0,0 +1,9 @@
+//
+//  SupportingFiles.swift
+//  ColorNumber
+//
+//  Created by Thuy Truong Quang on 12/26/17.
+//  Copyright © 2017 BigZero. All rights reserved.
+//
+
+import Foundation
