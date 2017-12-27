f05ac78fe8cf1199eb9950eaeff7c514477879ee
ChungTran <chungtv@tadi.io>
2017-12-27 09:59:27 +0700

    Init base controller and UI

---
+++ ColorNumber/Controllers/ImportViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ImportViewController.swift
+//  ColorNumber
+//
+//  Created by ChungTran on 12/27/17.
+//  Copyright © 2017 BigZero. All rights reserved.
+//
+
+import UIKit
+
+class ImportViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
