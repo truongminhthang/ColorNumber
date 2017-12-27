e9f4163836981a24fc23e85f3bb5a1decbd1615b
ChungTran <chungtv@tadi.io>
2017-12-27 10:52:30 +0700

    add action navigation libraryVC

--- ColorNumber/Controllers/LibraryViewController.swift
+++ ColorNumber/Controllers/LibraryViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 
 class LibraryViewController: UIViewController {
 
+    @IBOutlet weak var addThree: UIImageView!
+    @IBOutlet weak var watchVideo: UIImageView!
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        setupView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +25,20 @@ class LibraryViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: Setup view
+    func setupView() {
+        let tapWatchVideoImage = UITapGestureRecognizer.init(target: self, action: #selector(self.watchVideo(_:)))
+        watchVideo.addGestureRecognizer(tapWatchVideoImage)
+        let tapAddThree = UITapGestureRecognizer.init(target: self, action: #selector(self.addThree(_:)))
+        addThree.addGestureRecognizer(tapAddThree)
     }
-    */
-
+    
+    // MARK: Action view
+    @objc func watchVideo(_ recogznier: UITapGestureRecognizer) {
+        
+    }
+    
+    @objc func addThree(_ recogznier: UITapGestureRecognizer) {
+        
+    }
 }
