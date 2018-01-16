059a48f7944738ac248fac4337a5e41368e3333e
Truong Thang <thangtruong.dev@gmail.com>
2018-01-16 11:09:15 +0700

    add Gesture imagepixelview

--- ColorNumber/Models/ImageServices/Pixel.swift
+++ ColorNumber/Models/ImageServices/Pixel.swift
@@ -10,44 +10,113 @@ import UIKit
 /** Represents the memory address of a pixel. */
 typealias PixelPointer = UnsafePointer<UInt8>
 
+enum PixelType {
+    case number, color
+}
+
 /** A point in an image converted to an ASCII character. */
 class Pixel : UILabel {
     /** The number of bytes a pixel occupies. 1 byte per channel (RGBA). */
-    var size = CGSize(width: 50, height: 50)
+    static var size = CGSize(width: 10, height: 10)
+    var type: PixelType
     var rowIndex: Int
     var columnIndex: Int
     var red: UInt8 = 0
     var green: UInt8 = 0
     var blue: UInt8 = 0
-    var intensity: Double? {
-        didSet {
-            if let aIntensity = intensity {
-                text = "\(Int(aIntensity * 10))"
-                backgroundColor = UIColor.black.withAlphaComponent(1-CGFloat(Int(aIntensity * 10))/CGFloat(10))
-            } else {
-                text = ""
-            }
-        }
-    }
+    var intensity: Double
     var isFillColor = false
         
-    fileprivate let offset: Int
-    init(pointer: PixelPointer, offset: Int, row: Int, col: Int) {
-        self.offset = offset
+    init(red: UInt8, green: UInt8, blue: UInt8, intensity: Double, row: Int, col: Int, type: PixelType) {
+        self.type = type
         rowIndex = row
         columnIndex = col
-        red   =  pointer[offset + 0]
-        green =  pointer[offset + 1]
-        blue  =  pointer[offset + 2]
-        let frame = CGRect(origin: CGPoint(x: CGFloat(col) * size.width, y: CGFloat(row) * size.height), size: size)
+        self.red = red
+        self.blue = blue
+        self.green = green
+        self.intensity = intensity
+        let frame = CGRect(origin: CGPoint(x: CGFloat(col) * Pixel.size.width, y: CGFloat(row) * Pixel.size.height), size: Pixel.size)
         super.init(frame: frame)
-        layer.borderColor = UIColor.black.cgColor
-        layer.borderWidth = 0.1
         self.textAlignment = .center
+        self.font = UIFont.systemFont(ofSize: 5)
+        switch type {
+        case .number:
+            let displayIntensity = Int(intensity * 10)
+            if displayIntensity == 10 {
+                layer.borderColor = UIColor.lightGray.cgColor
+                layer.borderWidth = 0.1
+                text = ""
+            } else {
+                layer.borderColor = UIColor.black.cgColor
+                layer.borderWidth = 0.1
+                text = String(displayIntensity)
+            }
+            
+        case .color:
+            text = ""
+            fillGrayColor()
+        }
+       
         
+
+       
+    }
+    convenience init(pointer: PixelPointer, offset: Int, row: Int, col: Int, type: PixelType) {
+        
+        let red   =  pointer[offset + 0]
+        let green =  pointer[offset + 1]
+        let blue  =  pointer[offset + 2]
+        let intensity = Pixel.calculateIntensity(red: red, green: green, blue: blue)
+        self.init(red: red, green: green, blue: blue, intensity: intensity, row: row, col: col, type: type)
+
+        
+    }
+    
+    convenience init(_ pixel: Pixel) {
+        self.init(red: pixel.red, green: pixel.green, blue: pixel.blue, intensity: pixel.intensity, row: pixel.rowIndex, col: pixel.columnIndex, type: .color)
+    }
+    
+    func copy() -> Pixel
+    {
+        return Pixel(self)
+    }
+    
+    
+    func redrawFixel() {
+        if isFillColor == false {
+                fillGrayColor()
+        } else {
+            fillActualColor()
+        }
+    }
+    
+    fileprivate func fillGrayColor() {
+        backgroundColor = UIColor.black.withAlphaComponent(1-CGFloat(Int(intensity * 10))/CGFloat(10))
+    }
+    
+    fileprivate func fillActualColor() {
+        backgroundColor = UIColor(red: CGFloat(red) / 255, green: CGFloat(green) / 255, blue: CGFloat(blue) / 255, alpha: 1)
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    static func calculateIntensity(red: UInt8, green: UInt8, blue: UInt8) -> Double {
+        // Normalize the pixel's grayscale value to between 0 and 1.
+        // Weights from http://en.wikipedia.org/wiki/Grayscale#Luma_coding_in_video_systems
+        let
+        redWeight   = 0.229,
+        greenWeight = 0.587,
+        blueWeight  = 0.114,
+        weightedMax = 255.0 * redWeight   +
+            255.0 * greenWeight +
+            255.0 * blueWeight,
+        weightedSum = Double(red ) * redWeight   +
+            Double(green ) * greenWeight +
+            Double(blue) * blueWeight
+        return weightedSum / weightedMax
+        
+//        PixelImageView.patternColors[Int(intensity * 10)] = UIColor(red: CGFloat(pixel.red) / 255, green: CGFloat(pixel.green) / 255, blue: CGFloat(pixel.blue) / 255, alpha: 1)
     }
 }
--- ColorNumber/Models/LibraryModels/Image.swift
+++ ColorNumber/Models/LibraryModels/Image.swift
@@ -31,53 +31,124 @@ struct MapIntensityColor {
 
 
 
-class Image {
+class PixelImageView : UIView {
     var isEdited = false
+    var colorView: UIView = UIView()
+    var numberView: UIView = UIView()
     var image : UIImage
     var width: Int
     var height: Int
-    static var patternColors = [UIColor](repeating: UIColor.white, count: 11)
+    var patternColors = [UIColor](repeating: UIColor.white, count: 11)
     subscript (rowIndex: Int, heightIndex:Int) -> Pixel {
-        return pixels[rowIndex][heightIndex]
+        return pixelsNumber[rowIndex][heightIndex]
     }
-    var pixels : [[Pixel]] = []
+    var pixelsNumber : [[Pixel]] = []
+    var pixelColor: [[Pixel]] = []
+    
+    var zoomScaleForRemovingColor : CGFloat = 0.5
+    var zoomScaleForRemovingTextLabel: CGFloat = 0.8
+    
+    // Zoom in - Zoom out logic
+    var actualScalingParamter : CGFloat = 0 {
+        didSet {
+            if actualScalingParamter > zoomScaleForRemovingColor && oldValue < actualScalingParamter {
+                // occur When zoom in
+                colorView.alpha = 1 - (actualScalingParamter - zoomScaleForRemovingColor)
+                numberView.alpha = (actualScalingParamter - zoomScaleForRemovingColor)
+                isUserInteractionEnabled = true
+            }
+            else if actualScalingParamter > zoomScaleForRemovingColor && oldValue > actualScalingParamter {
+                // occurt when zoom out
+                colorView.alpha = 1 - (actualScalingParamter - zoomScaleForRemovingColor)
+                numberView.alpha = (actualScalingParamter - zoomScaleForRemovingColor)
+            }
+            else if actualScalingParamter <= zoomScaleForRemovingColor {
+                // occurt when normal
+                colorView.alpha = 1
+                numberView.alpha = 0
+                isUserInteractionEnabled = false
+            }
+            
+            
+        }
+    }
     init(image: UIImage) {
         self.image = image
         self.width = Int(image.size.width)
         self.height = Int(image.size.height)
-        
+        let width = (image.size.width) * Pixel.size.width
+        let height = (image.size.height) * Pixel.size.height
+        let imageSize = CGRect(origin: .zero, size: CGSize(width: width, height: height))
+        super.init(frame: imageSize)
+        setupSubview()
+        createPixelMatrix()
+        setupGesture()
+    }
+    
+    func createPixelMatrix() {
         let dataProvider = image.cgImage?.dataProvider
         let pixelData    = dataProvider?.data
         let pixelPointer = CFDataGetBytePtr(pixelData)
         let bytesPerPixel = 4
-        pixels = (0..<self.height).map { row in
-            (0..<self.width).map { col in
-                let offset = ((width * row) + col) * bytesPerPixel
-                let pixel = Pixel(pointer: pixelPointer!, offset: offset, row: row,  col: col)
-                self.calculateIntensity(for: pixel)
-                return pixel
+        for row in (0..<self.height) {
+            pixelsNumber.append([])
+            pixelColor.append([])
+            for col in (0..<self.width) {
+                let offset = ((self.width * row) + col) * bytesPerPixel
+                let numberPixel = Pixel(pointer: pixelPointer!, offset: offset, row: row,  col: col, type: .number)
+                numberView.addSubview(numberPixel)
+                colorView.addSubview(numberPixel.copy())
+                pixelsNumber[row].append(numberPixel)
+                pixelColor[row].append(numberPixel.copy())
+                
             }
         }
+        
     }
     
-    func calculateIntensity(for pixel: Pixel) {
-        // Normalize the pixel's grayscale value to between 0 and 1.
-        // Weights from http://en.wikipedia.org/wiki/Grayscale#Luma_coding_in_video_systems
-        let
-        redWeight   = 0.229,
-        greenWeight = 0.587,
-        blueWeight  = 0.114,
-        weightedMax = 255.0 * redWeight   +
-            255.0 * greenWeight +
-            255.0 * blueWeight,
-        weightedSum = Double(pixel.red ) * redWeight   +
-            Double(pixel.green ) * greenWeight +
-            Double(pixel.blue ) * blueWeight
-        let intensity = weightedSum / weightedMax
-        pixel.intensity =  intensity
-        Image.patternColors[Int(intensity * 10)] = UIColor(red: CGFloat(pixel.red) / 255, green: CGFloat(pixel.green) / 255, blue: CGFloat(pixel.blue) / 255, alpha: 1)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
-
+    
+    func setupSubview() {
+        colorView.frame = self.bounds
+        numberView.frame = self.bounds
+        self.addSubview(colorView)
+        self.insertSubview(numberView, aboveSubview: colorView)
+    }
+    
+    private func setupGesture() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(sender:)))
+        tapGestureRecognizer.delegate = self
+        addGestureRecognizer(tapGestureRecognizer)
+        let dragGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleDrag(sender:)))
+        dragGestureRecognizer.minimumPressDuration = 0.25
+        addGestureRecognizer(dragGestureRecognizer)
+    }
+    
+    
+    @objc private func handleTap(sender: UIGestureRecognizer) {
+        self.draw(atPoint: sender.location(in: self))
+    }
+    
+    @objc private func handleDrag(sender: UIGestureRecognizer) {
+        switch sender.state {
+        case .changed, .began:
+            self.draw(atPoint: sender.location(in: self))
+        default: break
+        }
+    }
+    private func draw(atPoint point: CGPoint) {
+        let y = Int(point.y / Pixel.size.height)
+        let x = Int(point.x / Pixel.size.width)
+        guard x < self.pixelsNumber[0].count && y < self.pixelsNumber.count && y >= 0 && x >= 0 else { return }
+        pixelsNumber[y][x].backgroundColor = UIColor.red
+        pixelColor[y][x].backgroundColor = UIColor.red
+    }
 }
 
+extension PixelImageView: UIGestureRecognizerDelegate {
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+}
