aed0502d311600d213d7f62305d4e6299c681892
Truong Thang <thangtruong.dev@gmail.com>
2018-02-01 00:15:23 +0700

    update googleadsmod sdk

--- ColorNumber/AppDelegate.swift
+++ ColorNumber/AppDelegate.swift
@@ -48,6 +48,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GoogleAdMob.sharedInstance.showInterstitial()
         return true
     }
 
